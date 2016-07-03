7b04e4fd3f6bc8e25e85af697517890da2a29e1c
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-07-03 11:39:29 +0800

    Add virtual field full name

--- models/student.php
+++ models/student.php
@@ -1,6 +1,8 @@
 <?php
 class Student extends AppModel {
 	var $name = 'Student';
+	var $virtualFields= array('full_name'=>'CONCAT(Student.first_name," ",Student.last_name)');
+	var $displayField ='full_name';
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 
 	var $hasMany = array(
