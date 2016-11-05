ce209f302937711e9d5a63869c01dc30eef84396
Dave Arroyo <arroyo.daveil@gmail.com>
2016-11-06 07:47:04 +0800

    Update models and controllers to accomodate registration changes

--- models/student.php
+++ models/student.php
@@ -2,8 +2,26 @@
 class Student extends AppModel {
 	var $name = 'Student';
 	var $virtualFields= array('full_name'=>'CONCAT(Student.first_name," ",Student.last_name)');
-	var $displayField ='full_name';
+    var $displayField ='full_name';
+
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+	var $belongsTo = array(
+		'User' => array(
+			'className' => 'User',
+			'foreignKey' => 'user_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'Section' => array(
+			'className' => 'Section',
+			'foreignKey' => 'section_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
 
 	var $hasMany = array(
 		'Evaluation' => array(
